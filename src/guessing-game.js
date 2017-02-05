32faa78dff9ed78250d7d488fa6e9e4e48c4fbe0
antondrazdou <woodland.by@gmail.com>
2017-02-05 23:24:01 +0300

    game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {     
+        this.min = 0;      
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.middleOfRange();
     }
 
     lower() {
-
+        this.max = this.middleOfRange();
     }
 
     greater() {
+        this.min = this.middleOfRange();
+    }
 
+    /**
+     * find index of the middle of range
+     * @returns {number} index of the middle of current range
+     */
+    middleOfRange() {
+        return Math.ceil((this.min + this.max) / 2);
     }
 }
 
